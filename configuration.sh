2187badc895f8c82910ca5b0866927c06034ef77
sirselim <miles.benton84@gmail.com>
2018-04-26 14:03:51 +1200

    testing updated configuration options

--- configuration.sh
+++ configuration.sh
@@ -10,7 +10,8 @@ THREADS="12"
 # set paths to software
 SNPEFF="/data/all/programs/snpeff/snpEff/snpEff.jar"
 SNPSIFT="/data/all/programs/snpeff/snpEff/SnpSift.jar"
-VEP="/data/all/programs/VEP/vep-87-github-release/vep.pl"
+#VEP="/data/all/programs/VEP/vep-87-github-release/vep.pl"
+VEP="/data/all/programs/VEP/ensembl-vep/vep"
 BGZIP="/usr/local/bin/bgzip"
 TABIX="/usr/local/bin/tabix"
 BCFTOOLS="/usr/bin/bcftools"
@@ -18,7 +19,8 @@ VCFSORT="/usr/bin/vcf-sort"
 
 # set paths to database directories
 DBSNP_DATA="/data/all/ncbi/hg19/All_20170710.vcf.gz"
-VEP_DATA="/data/all/VEPdata/"
+#VEP_DATA="/data/all/VEPdata/"
+VEP_DATA="/data/all/VEPdata/homo_sapiens/92_GRCh37/"
 FASTA_REF="/data/all/VEPdata/homo_sapiens/89_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa"
 DBNSFP="/data/all/dbNSFP/hg19/v2.9.3/dbNSFPv2.9.3_hg19_custom.txt.gz"
 
