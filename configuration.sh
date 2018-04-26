58cce7260edec3cb5c96bdc3aaf7cfadff0b52fd
sirselim <miles.benton84@gmail.com>
2018-04-26 14:18:37 +1200

    testing updated configuration options

--- configuration.sh
+++ configuration.sh
@@ -19,8 +19,7 @@ VCFSORT="/usr/bin/vcf-sort"
 
 # set paths to database directories
 DBSNP_DATA="/data/all/ncbi/hg19/All_20170710.vcf.gz"
-#VEP_DATA="/data/all/VEPdata/"
-VEP_DATA="/data/all/VEPdata/homo_sapiens/92_GRCh37/"
+VEP_DATA="/data/all/VEPdata/"
 FASTA_REF="/data/all/VEPdata/homo_sapiens/89_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa"
 DBNSFP="/data/all/dbNSFP/hg19/v2.9.3/dbNSFPv2.9.3_hg19_custom.txt.gz"
 
