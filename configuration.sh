528619e17de956f0d94f016d31bc270daf5eeb96
sirselim <miles.benton84@gmail.com>
2018-05-14 10:07:08 +1200

    updated config for remote server testing

--- configuration.sh
+++ configuration.sh
@@ -10,48 +10,48 @@ THREADS="6"
 ####
 # set paths to software
 ## taurus
-# SNPEFF="/data/all/programs/snpeff/snpEff/snpEff.jar"
-# SNPSIFT="/data/all/programs/snpeff/snpEff/SnpSift.jar"
-# VEP="/data/all/programs/VEP/ensembl-vep/vep"
-# BGZIP="/usr/local/bin/bgzip"
-# TABIX="/usr/local/bin/tabix"
-# BCFTOOLS="/usr/bin/bcftools"
-# VCFSORT="/usr/bin/vcf-sort"
-## wagyu
-SNPEFF="/data2/programs/snpeff/snpEff.jar"
-SNPSIFT="/data2/programs/snpeff/SnpSift.jar"
-VEP="/data2/programs/ensembl-vep/vep"
-BGZIP="/usr/bin/bgzip"
-TABIX="/usr/bin/tabix"
+SNPEFF="/data/all/programs/snpeff/snpEff/snpEff.jar"
+SNPSIFT="/data/all/programs/snpeff/snpEff/SnpSift.jar"
+VEP="/data/all/programs/VEP/ensembl-vep/vep"
+BGZIP="/usr/local/bin/bgzip"
+TABIX="/usr/local/bin/tabix"
 BCFTOOLS="/usr/bin/bcftools"
 VCFSORT="/usr/bin/vcf-sort"
+## wagyu
+# SNPEFF="/data2/programs/snpeff/snpEff.jar"
+# SNPSIFT="/data2/programs/snpeff/SnpSift.jar"
+# VEP="/data2/programs/ensembl-vep/vep"
+# BGZIP="/usr/bin/bgzip"
+# TABIX="/usr/bin/tabix"
+# BCFTOOLS="/usr/bin/bcftools"
+# VCFSORT="/usr/bin/vcf-sort"
 ####
 
 ####
 # set paths to database directories
-#DBSNP_DATA="/data/all/ncbi/hg19/All_20170710.vcf.gz"
-#VEP_DATA="/data/all/VEPdata/"
+DBSNP_DATA="/data/all/ncbi/hg19/All_20170710.vcf.gz"
+VEP_DATA="/data/all/VEPdata/"
 ##FASTA_REF="/data/all/VEPdata/homo_sapiens/89_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa"
 ## when you build a new version of VEP you might need to manually delete the index file (permission issues)
-#FASTA_REF="/home/grcnata/fasta/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa"
+FASTA_REF="/home/grcnata/fasta/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa"
 ## older dbNSFP version [hg19]
-#DBNSFP="/data/all/dbNSFP/hg19/v2.9.3/dbNSFPv2.9.3_hg19_custom.txt.gz"
+DBNSFP="/data/all/dbNSFP/hg19/v2.9.3/dbNSFPv2.9.3_hg19_custom.txt.gz"
 ## newest dbNSFP version [hg19]
 ## DBNSFP="/data/all/dbNSFP/hg19/v3.5a/dbNSFPv3.5a.hg19.txt.gz"
 ## newest dbNSFP version [hg38]
 ## DBNSFP="/data/all/dbNSFP/hg38/v3.5a/dbNSFPv3.5a.txt.gz"
 ## wagyu
-DBSNP_DATA="/data2/dbSNP/hg19/All_20170710.vcf.gz"
-VEP_DATA="/data2/VEPdata/"
-FASTA_REF="/data2/VEPdata/homo_sapiens/92_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz"
-DBNSFP="/data2/dbNSFP/hg19/v2.9.3/dbNSFPv2.9.3_hg19_custom.txt.gz"
+# DBSNP_DATA="/data2/dbSNP/hg19/All_20170710.vcf.gz"
+# VEP_DATA="/data2/VEPdata/"
+# FASTA_REF="/data2/VEPdata/homo_sapiens/92_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz"
+# DBNSFP="/data2/dbNSFP/hg19/v2.9.3/dbNSFPv2.9.3_hg19_custom.txt.gz"
 ####
 
 ####
 # dir of sequence and quality/coverage data
 ## taurus
-# RAWDATA="/home/grcnata/raw_wes_files/"
+RAWDATA="/home/grcnata/raw_wes_files/"
 
 ## wagyu
-RAWDATA="/data2/vcfdart_data/"
+# RAWDATA="/data2/vcfdart_data/"
 ####
